731bee842c9d73ccc831d8d88a9ccab8a890ca0c
Ryan McNair <ryan.mcnair@gmail.com>
2022-10-18 17:20:23 -0500

    finish module 3

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,14 @@
 function startGame() {
     // starting a new game
-    var messagesElement = document.getElementById('messages');
+    let playerName: string = 'Audrey';
+    logPlayer(playerName);
+    
+    let messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`)
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
