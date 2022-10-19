1ac8ee11a5d11c42a9f0579550a237688547b981
Ryan McNair <ryan.mcnair@gmail.com>
2022-10-19 11:40:36 -0500

    finished module 4

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,46 @@
 function startGame() {
     // starting a new game
-    let playerName: string = 'Audrey';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
     
-    let messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'
+    postScore(80, playerName);
+    postScore(-5, playerName);
 }
 
-function logPlayer(name: string) {
+function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`)
 }
 
+function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+
+    if (inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+    let logger: (value: string) => void;
+
+    if (score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage;
+    }
+
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+logMessage('Welcome to MultiMath');
+
+function logError(err: string): void {
+    console.error(err);
+}
