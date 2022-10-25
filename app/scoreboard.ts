da2d85bcfc01fb49df76c8f95edf7a7735c0c7e5
Ryan McNair <ryan.mcnair@gmail.com>
2022-10-25 18:58:56 -0500

    completed course

--- app/scoreboard.ts
+++ app/scoreboard.ts
@@ -1,11 +1,14 @@
-/// <reference path="result.ts" />
+import { Result } from './result';
+import * as _ from "lodash";
 
-class Scoreboard {
+export class Scoreboard {
 
   private results: Result[] = [];
 
   addResult(newResult: Result): void {
     this.results.push(newResult);
+    let allCapsName: string = _.upperCase(newResult.playerName);
+    console.log(`${allCapsName}: ${newResult.score}`);
   }
 
   updateScoreboard(): void {
